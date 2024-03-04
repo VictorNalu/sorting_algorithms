1e9a20f49b6a83e677c192bb667275e8b194358d
Pam-123 <pamschawira@gmail.com>
2024-03-04 06:32:29 -0800

    All

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,7 +6,7 @@ void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 
 /**
- * swap_ints - Swap two integers in an array.
+ * swap - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
