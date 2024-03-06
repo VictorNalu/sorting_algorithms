d308fe3a16e5ed42bf6d1362c818dff7cc23f10e
VictorNalu <victorchinalurumogu@gmail.com>
2024-03-06 09:34:34 +0100

    merge_sort

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_list(const listint_t *list);
 void counting_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 #endif
